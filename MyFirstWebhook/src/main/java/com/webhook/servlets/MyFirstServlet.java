5dcffe7d7706867eba1c3e180d78272e1b35cafb
Sumashri Gupta <Sumashri Gupta@DESKTOP-C3FCOSI>
2023-02-26 23:12:00 +0530

    test commit

--- MyFirstWebhook/src/main/java/com/webhook/servlets/MyFirstServlet.java
+++ MyFirstWebhook/src/main/java/com/webhook/servlets/MyFirstServlet.java
@@ -33,6 +33,7 @@ public class MyFirstServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
+		
 	}
 
 }
