78c48da56352c546cdd80321bc62ee4c45e5dfd2
Sumashri Gupta <Sumashri Gupta@DESKTOP-C3FCOSI>
2023-02-26 23:41:10 +0530

    test commit

--- MyFirstWebhook/src/main/java/com/webhook/servlets/MyFirstServlet.java
+++ MyFirstWebhook/src/main/java/com/webhook/servlets/MyFirstServlet.java
@@ -1,6 +1,10 @@
 package com.webhook.servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,7 +36,43 @@ public class MyFirstServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		// doGet(request, response);
+		String data = getBody(request);
+		System.out.println(data);
+	}
+	
+	public String getBody(HttpServletRequest request) throws IOException {
+
+		System.out.println("Inside getBody");
+		String body = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = null;
+
+		try {
+			InputStream inputStream = request.getInputStream();
+			if (inputStream != null) {
+				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+				char[] charBuffer = new char[128];
+				int bytesRead = -1;
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+					stringBuilder.append(charBuffer, 0, bytesRead);
+				}
+			} else {
+				stringBuilder.append("");
+			}
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException ex) {
+					throw ex;
+				}
+			}
+		}
+		body = stringBuilder.toString();
+		return body;
 	}
 
 }
